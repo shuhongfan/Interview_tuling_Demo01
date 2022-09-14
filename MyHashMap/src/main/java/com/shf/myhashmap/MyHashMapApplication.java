c91d07c25d19ca734b8dddb5f2f6605eec9cb583
shuhongfan <shfshuhongfan@gmail.com>
2022-09-14 22:51:15 +0800

    init

---
+++ MyHashMap/src/main/java/com/shf/myhashmap/MyHashMapApplication.java
@@ -0,0 +1,13 @@
+package com.shf.myhashmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyHashMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyHashMapApplication.class, args);
+    }
+
+}
---
+++ MyHashMap/src/test/java/com/shf/myhashmap/MyHashMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shf.myhashmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyHashMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
